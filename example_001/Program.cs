ea7a5f384aea9362a0d20ed2ec115f82c4ee45ed
CJINBNH <alex_egorov_89@mail.ru>
2022-08-30 20:56:14 +0400

    Собрать строку от a до b. Найти сумму чисел от 1 до n

--- example_001/Program.cs
+++ example_001/Program.cs
@@ -1,2 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Собрать строку с числами от a до b, a <= b
+
+// Console.Clear();
+
+// string NumbersFor(int a, int b)
+// {
+//     string result = String.Empty;
+//     for (int i = a; i <= b; i++)
+//     {
+//         result += $"{i} ";
+//     }
+//     return result;
+// }
+
+// string NumbersRec(int a, int b)
+// {
+//     if (a <= b) return $"{a} " + NumbersRec(a + 1, b);
+//     else return String.Empty;
+// }
+
+// Console.WriteLine(NumbersFor(1, 10)); // 1 2 3 4 5 6 7 8 9 10
+// Console.WriteLine(NumbersRec(1, 10)); // 1 2 3 4 5 6 7 8 9 10
+
+// Собрать строку с числами от a до b, a >= b
+
+// Console.Clear();
+
+// string NumbersFor(int a, int b)
+// {
+//     string result = String.Empty;
+//     for (int i = b; i>= a; i--)
+//     {
+//         result += $"{i} ";
+//     }
+//     return result;
+// }
+
+// string NumbersRec(int a, int b)
+// {
+//     if (a <= b) return NumbersRec(a + 1, b) + $"{a} ";
+//     else return String.Empty;
+// }
+
+// Console.WriteLine(NumbersFor(1, 10)); // 10 9 8 7 6 5 4 3 2 1
+// Console.WriteLine(NumbersRec(1, 10)); // 10 9 8 7 6 5 4 3 2 1s
+
+// Найти сумму чисел от 1 до n
+
+// Console.Clear();
+
+// int SumFor(int n)
+// {
+//     int result = 0;
+//     for (int i = 1; i <= n; i++) result += i;
+//     return result;
+// }
+// Console.WriteLine(SumFor(10)); // 55
+
+// int SumRec(int n)
+// {
+//     if (n == 0) return 0;
+//     else return n + SumRec(n - 1);
+// }
+// Console.WriteLine(SumRec(10));
