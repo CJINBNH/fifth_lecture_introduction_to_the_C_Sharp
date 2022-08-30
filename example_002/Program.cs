e2e09c2fb224d3b5fd9c94335f612ef30d8ac55b
CJINBNH <alex_egorov_89@mail.ru>
2022-08-30 21:17:52 +0400

    Факториал числа. Вычислить a в степени n

--- example_002/Program.cs
+++ example_002/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Факториал числа
+
+// Console.Clear();
+
+// int FactorialFor(int n)
+// {
+//     int result = 1;
+//     for (int i = 1; i <= n; i++) result *= i;
+//     return result;
+// }
+
+// Console.WriteLine(FactorialFor(10));
+
+// int FactorialRec(int n)
+// {
+//     if (n == 1 || n == 0) return 1;
+//     else return n * FactorialRec(n - 1);
+// }
+// Console.WriteLine(FactorialRec(0));
+
+// Вычислить a в степени n
+
+Console.Clear();
+
+int PowerFor(int a, int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        result *= a;
+    }
+    return result;
+}
+Console.WriteLine(PowerFor(3, 5));
+
+int PowerRec(int a, int n)
+{
+    if (n == 0) return 1;
+    else return a * PowerRec(a, n-1);
+}
+Console.WriteLine(PowerRec(3,5));
