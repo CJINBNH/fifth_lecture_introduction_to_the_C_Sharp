f3e75ecac43d6ec73590272f0628c377421fcfe0
CJINBNH <alex_egorov_89@mail.ru>
2022-08-30 21:23:40 +0400

    Добавили решение для четной степени

--- example_002/Program.cs
+++ example_002/Program.cs
@@ -39,3 +39,11 @@ int PowerRec(int a, int n)
     else return a * PowerRec(a, n-1);
 }
 Console.WriteLine(PowerRec(3,5));
+
+int PowerRecMath(int a, int n)
+{
+     if (n == 0) return 1;
+     else if (n % 2 == 0) return PowerRecMath(a * a, n / 2);
+     else return PowerRecMath(a, n - 1) * a;
+}
+Console.WriteLine(PowerRecMath(3,5));
